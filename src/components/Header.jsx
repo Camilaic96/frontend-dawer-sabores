c91d0d7f4cd956db11f5daf2cf4d7d205bb6fefb
camila cuello <camilaicuello@hotmail.com>
2023-06-22 16:15:05 -0300

    Header - without adding NavBar + favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <header>
-      Header
+    <header className="banner-navbar blue-background">
+      <div className="banner-content">
+        <img src="img/logo.png" alt="Logo" className="logo" />
+      </div>
       <NavBar />
     </header>
   );
