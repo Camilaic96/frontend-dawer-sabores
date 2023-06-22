9d3eb17932e4dd23905af7304bc7edf2a4dc416a
camila cuello <camilaicuello@hotmail.com>
2023-06-22 17:25:15 -0300

    add about us + faqs

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,36 @@
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div className="text-center" id="about-us">
+      <h2 className="p-1 blue-background">Quiénes Somos</h2>
+      <div className="p-3">
+        <p>
+          <span className="bold">Dawer Sabores</span> es un emprendimiento que
+          nació en plena pandemia para generar nuevos ingresos.
+        </p>
+        <p>
+          La venta de vinos y productos comestibles de{" "}
+          <span className="bold">excelencia procedencia y calidad</span> es lo
+          que nos caracteriza.
+        </p>
+        <p>
+          Lo que comenzó como una opción para sobrellevar un momento difícil
+          para todos, se convirtió en un negocio que{" "}
+          <span className="bold">crece</span> y en el que{" "}
+          <span className="bold">disfrutamos</span> mucho trabajar.
+        </p>
+        <p>
+          Hoy, te brindamos un espacio donde podés visualizar los productos que
+          tenemos en el mercado, conociendo el origen y carasterísticas de cada
+          uno.
+        </p>
+        <p>
+          <span className="bold">
+            Te ofrecemos atención personalizada y te garantizamos calidad.
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,5 +1,50 @@
 const FAQs = () => {
-  return <div>FAQs</div>;
+  return (
+    <div className="m-5 text-center" id="faqs">
+      <h2>¿Cómo realizo mi pedido?</h2>
+      <div className="text-start">
+        <div className="d-flex m-3">
+          <img
+            src="img/icons/search-blue.png"
+            alt="search icon"
+            className="icon-faqs mx-3"
+          />
+          <p className="text-justify mx-3">
+            Buscá el producto de tu interés. Podés encontrarlo desde la lupa o
+            categorías. Además, podés aplicar filtros dentro de cada una para
+            que te resulte más fácil y rápido.
+          </p>
+        </div>
+        <div className="d-flex m-3">
+          <p className="text-justify mx-3">
+            Una vez que lo encuentres, seleccionalo y sumalo a tu lista. Podés
+            elegir cuántas unidades querés, sólo recordá que cada producto está
+            sujeto a disponibilidad de stock.
+          </p>
+          <img
+            src="img/icons/check-square-blue.png"
+            alt="check square icon"
+            className="icon-faqs mx-3"
+          />
+        </div>
+        <div className="d-flex m-3">
+          <img
+            src="img/icons/send-plane-line-blue.png"
+            alt="send plane line blue icon"
+            className="icon-faqs mx-3"
+          />
+          <p className="text-justify mx-3">
+            Finalizada la lista sólo queda enviarla. Nos llegará por Whatsapp y
+            nos estaremos contactando a la brevedad para finalizar el pedido,
+            acordar el medio de pago y la entrega.
+          </p>
+        </div>
+      </div>
+      <a href="https://wa.me/+5491154998486" rel="noreferrer" target="_blank">
+        <p className="text-end">¿Tenés alguna otra duda? Consultanos</p>
+      </a>
+    </div>
+  );
 };
 
 export default FAQs;
