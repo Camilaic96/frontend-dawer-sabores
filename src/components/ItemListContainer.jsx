32c5bae4949334276f6cbd3f13a3ac675e0a6736
camila cuello <camilaicuello@hotmail.com>
2023-07-17 18:09:27 -0300

    add JSON products

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -6,84 +6,84 @@ const ItemListContainer = ({ categoria }) => {
       nombre: "Vino Atilio",
       variedad: "Malbec",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega Atilio Avena",
+      productor: "Bodega Atilio Avena",
       categoria: "vinos-y-bebidas",
     },
     {
       nombre: "Vino Atilio",
       variedad: "Cabernet Sauvignon",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega Atilio Avena",
+      productor: "Bodega Atilio Avena",
       categoria: "vinos-y-bebidas",
     },
     {
       nombre: "Prófugo Especias",
       variedad: "Cabernet",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Casa de la primavera bodegas y viñedos",
+      productor: "Casa de la primavera bodegas y viñedos",
       categoria: "vinos-y-bebidas",
     },
     {
       nombre: "Pimientos morrones",
       variedad: "",
       presentacion: "Frasco 295 gr.",
-      clasificacion: "Línea Illapel",
+      productor: "Línea Illapel",
       categoria: "dulces-y-encurtidos",
     },
     {
       nombre: "Nuez Chandler Mariposa Extra Light",
       variedad: "",
       presentacion: "1 kg.",
-      clasificacion: "Frutos secos, desecados y confituras",
+      productor: "Frutos secos, desecados y confituras",
       categoria: "frutos-y-cereales",
     },
     {
       nombre: "prod 6",
       variedad: "Malbec",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega Atilio Avena",
+      productor: "Bodega Atilio Avena",
       categoria: "panificados-e-insumos",
     },
     {
       nombre: "Cerezas al maraschino",
       variedad: "",
       presentacion: "Frasco 170gr.",
-      clasificacion: "Línea Illapel",
+      productor: "Línea Illapel",
       categoria: "dulces-y-encurtidos",
     },
     {
       nombre: "Aji lococo en escabeche",
       variedad: "",
       presentacion: "360 gr.",
-      clasificacion: "Linea atacopampa",
+      productor: "Linea atacopampa",
       categoria: "dulces-y-encurtidos",
     },
     {
       nombre: "prod 5",
       variedad: "Malbec",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega catalina",
+      productor: "Bodega catalina",
       categoria: "panificados-e-insumos",
     },
     {
       nombre: "prod 6",
       variedad: "Malbec",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega Atilio Avena",
+      productor: "Bodega Atilio Avena",
       categoria: "panificados-e-insumos",
     },
     {
       nombre: "prod 7",
       variedad: "Malbec",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega catalina",
+      productor: "Bodega catalina",
       categoria: "especias-y-condimentos",
     },
     {
       nombre: "prod 8",
       variedad: "Malbec",
       presentacion: "Unidad 750 cc.",
-      clasificacion: "Bodega Atilio Avena",
+      productor: "Bodega Atilio Avena",
       categoria: "especias-y-condimentos",
     },
   ];
@@ -94,37 +94,35 @@ const ItemListContainer = ({ categoria }) => {
 
   const groupedProducts = {};
   filteredProducts.forEach((product) => {
-    if (!groupedProducts[product.clasificacion]) {
-      groupedProducts[product.clasificacion] = [];
+    if (!groupedProducts[product.productor]) {
+      groupedProducts[product.productor] = [];
     }
-    groupedProducts[product.clasificacion].push(product);
+    groupedProducts[product.productor].push(product);
   });
 
   return (
     <div className="p-3">
-      {Object.keys(groupedProducts).map((clasificacion) => (
-        <div key={clasificacion} className="container-classification">
-          <h4 className="title-classification">{clasificacion}</h4>
+      {Object.keys(groupedProducts).map((productor) => (
+        <div key={productor} className="container-classification">
+          <h4 className="title-classification">{productor}</h4>
           <div className="body-classification">
             <table className="table">
               <tbody>
-                {groupedProducts[clasificacion].map((product) => (
-                  <tr key={product.nombre}>
-                    <td
-                      scope="col"
-                      className="name-product-classification"
-                      colSpan={3}
-                    >
-                      {product.nombre}
-                    </td>
-                    <td scope="col" className="item-classification" colSpan={3}>
-                      | {product.variedad}
-                    </td>
-                    <td scope="col" className="item-classification" colSpan={3}>
-                      | {product.presentacion}
-                    </td>
-                    <td scope="col" className="item-classification" colSpan={3}>
-                      <div className="d-flex justify-content-end align-items-center">
+                {groupedProducts[productor].map((product) => (
+                  <tr key={product.nombre} className="mobile-row">
+                    <td colSpan={12}>
+                      <div className="mobile-info">
+                        <span className="name-product-classification">
+                          {product.nombre}
+                        </span>
+                        <span className="item-classification">
+                          | {product.variedad}
+                        </span>
+                        <span className="item-classification">
+                          | {product.presentacion}
+                        </span>
+                      </div>
+                      <div className="mobile-buttons">
                         <button className="btn-products">
                           <img
                             src={"../img/icons/minus-red.png"}
